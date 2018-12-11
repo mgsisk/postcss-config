dacaf8797696f360ff7717c406fc572f97f0c63e
Michael Sisk <me@mgsisk.com>
2018-12-10 20:19:07 -0500

    Add plugin

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const postcss = require('postcss')
+
+module.exports = postcss.plugin('@mgsisk/postcss-config', ()=> (_root, result)=> {
+  result.processor
+    .use(require('postcss-import'))
+    .use(require('postcss-normalize'))
+    .use(require('@mgsisk/postcss-modular-rhythm'))
+    .use(require('postcss-easings'))
+    .use(require('postcss-short'))
+    .use(require('postcss-preset-env')({
+      stage: 0,
+      features: {'custom-properties': {preserve: false}},
+    }))
+    .use(require('postcss-color-mod-function'))
+    .use(require('cssnano')({preset: 'default'}))
+})
